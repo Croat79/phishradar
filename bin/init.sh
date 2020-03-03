341c2e90b5dc77ae579990560108e03c64873bfa
tasooshi <tasooshi@pm.me>
2020-03-03 22:41:05 +0100

    Closes #6 Ask password

--- bin/init.sh
+++ bin/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+stty -echo
+printf "Administrator (admin) password: "
+read PASSWORD
+stty echo
+printf "\n"
+
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py makemigrations
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py migrate
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py collectstatic --no-input
-docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'admin')"
+docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '$PASSWORD')"
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py fetch_sources
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py init_sources
 docker-compose --env-file docker-environ run webapp /usr/local/bin/python /srv/webapp/manage.py loaddata ./fixtures/keywords.json
